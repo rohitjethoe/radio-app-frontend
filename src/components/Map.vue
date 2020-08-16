0c8d90a6c59788181198e2203db486525105887b
rohitjethoe <rohitchai@icloud.com>
2020-08-16 20:02:04 +0200

    Seperated front and back end in different repo's.

---
+++ src/components/Map.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div id="map">
+            coming soon...
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Map",
+}
+</script>
+
+<style scoped>
+</style>
