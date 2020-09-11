0a4b717c1d24850a2db0067b7c243444c218fd58
rohitjethoe <rohitchai@icloud.com>
2020-09-11 17:44:12 +0200

    Created updated version setup.

--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
   const routes = [
   {
     path: '/',
     name: 'Home',
     component: () => import('../views/Home.vue')
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: () => import('../views/Contact.vue')
-  },
-  {
-    path: '/policy',
-    name: 'Policy',
-    component: () => import('../views/Policy.vue')
-  }
 ]
 
 const router = new VueRouter({
