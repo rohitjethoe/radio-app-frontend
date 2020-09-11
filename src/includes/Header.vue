349d38ddea5b7e847fc1197ce2ea088cab3e2c9a
rohitjethoe <rohitchai@icloud.com>
2020-09-11 18:14:25 +0200

    Header done.

---
+++ src/includes/Header.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="header" v-bind:class="{ opened : navStatus}">
+      <div class="nav">
+        <div @click="openNavBar()" class="burger-icon">
+            <div class="rect rect-1"></div>
+            <div class="rect"></div>
+            <div class="rect rect-3"></div>
+        </div>
+      </div>
+      <div class="logo">
+        Radioworld
+      </div>
+      <div class="radio">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+            navStatus: false
+        }
+    },
+    methods: {
+        openNavBar() {
+            this.navStatus = !this.navStatus;
+        }
+    }
+}
+</script>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    .opened .rect-1 {
+        transform: translateY(8px);
+    }
+
+    .opened .rect-3 {
+        transform: translateY(-8px);
+    }
+
+    .burger-icon {
+        padding: 14px;
+    }
+
+    .burger-icon:hover {
+        cursor: pointer;
+    }
+
+    .burger-icon .rect {
+        width: 30px;
+        margin: 4.2px 0px;
+        height: 3.5px;
+        background-color: #fff;
+        transition: 0.2s all;
+    }
+
+    .nav {
+        float: left;
+    }
+
+    .logo {
+        text-align: center;
+        font-family: 'Teko', sans-serif;
+        font-size: 32px;
+        text-transform: uppercase;
+        padding-top: 5px;
+        color: #fff;
+    }
+
+    .radio {
+        float: right;
+        font-size: 24px;
+    }
+
+    .header::after {
+        clear: both;
+        content: "";
+        display: table;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="root">
-    
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from '@/includes/Header';
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Header
+  }
 }
 </script>
 
 <style>
-
+  .root {
+    background-color: #84C2C0;
+  }
 </style>
