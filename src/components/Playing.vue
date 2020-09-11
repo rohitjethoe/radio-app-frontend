c51b1c1229491e15f9422fae53dbeda12a02614e
rohitjethoe <rohitchai@icloud.com>
2020-09-11 20:34:24 +0200

    Added playing component.

---
+++ src/components/Playing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="playing">
+      <div class="title">
+
+      </div>
+      <div class="country">
+
+      </div>
+      <div class="continent">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Playing'
+}
+</script>
+
+<style>
+
+</style>
