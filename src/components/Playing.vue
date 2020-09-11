a2508764c6579f589fdfabbfc24b8a45a88d3c7b
rohitjethoe <rohitchai@icloud.com>
2020-09-11 21:40:50 +0200

    Added styling and structure of playing component.

--- src/components/Playing.vue
+++ src/components/Playing.vue
@@ -1,23 +1,48 @@
 <template>
   <div class="playing">
       <div class="title">
-
+        📻 {{ playing.name }}
       </div>
-      <div class="country">
-
-      </div>
-      <div class="continent">
-
+      <div class="radio">
+        <a target="_blank" rel="noopener" noreferrer :href="playing.url" class="radio-url">
+            Link to radio station source <i class="fas fa-external-link-alt"></i>
+        </a>
       </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Playing'
+    name: 'Playing',
+    props: ['playing']
 }
 </script>
 
 <style>
+    .playing {
+        background-color: #fff;
+        width: 80%;
+        padding: 15px 10px;
+        border-radius: 4px;
+        box-shadow: 0px 1.5px 3px rgba(0, 0, 0, 0.25);
+    }
 
+    .title {
+        font-family: 'Teko', sans-serif;
+        font-size: 36px;
+        color: #555;
+    }
+
+    .radio-url:link, .radio-url:visited {
+        font-family: 'Teko', sans-serif;
+        font-size: 18px;
+        text-decoration: none;
+        color: #84C2C0;
+        border-bottom: 2px solid #84C2C0;
+    }
+
+    .fas {
+        font-size: 16px;
+        margin-left: 5px;
+    }
 </style>
